7ea69eb2ed2d7ff5e56ded041307a1da205afdf0
Nisouzaa <souzanini@gmail.com>
2025-08-08 14:56:10 -0300

    projeto tabuada

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,27 @@
-console.log("teste");
+// seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+const multiplicationTable = document.querySelector(
+  "#multiplication-operations"
+);
+
+// funções
+
+const createTable = (number, multiplicatorNumber) => {
+  multiplicationTable.innerHTML = "";
+};
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const multiplicationNumber = +numberInput.value;
+
+  const multiplicatorNumber = +multiplicationInput.value;
+
+  if (!multiplicationNumber || !multiplicatorNumber) return;
+
+  console.log(multiplicationNumber, multiplicatorNumber);
+});
